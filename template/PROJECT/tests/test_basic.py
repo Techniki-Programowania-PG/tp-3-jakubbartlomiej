8dcd3c906a523db943e22f2c1019b3de87d20814
Bartłomiej Fedorowicz <bartek.gdy@gmail.com>
2025-05-30 00:28:40 +0200

    improved tests

--- template/PROJECT/tests/test_basic.py
+++ template/PROJECT/tests/test_basic.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 import scikit_build_example as m
 
-def test_add():
-    assert m.add(1, 2) == 3
+def test_sin():
+    m.sinPattern(0.16, 0, 6.28, 10000)
 
+def test_cos():
+    m.cosPattern(1, -3.1415, 3.1415, 1000)
 
-def test_sub():
-    assert m.subtract(1, 2) == -1
+def test_square():
+    m.squarePattern(1, -3.1415, 3.1415, 1000)
 
-m.sinPattern(1, 0, 3, 1000)
+def test_sawtooth():
+    m.sawtoothPattern(1, -3.1415, 3.1415, 1000)
